d7d9bf61619b1d70504e1493ba414dc39de38a35
Zuchis <liquimaster@gmail.com>
2015-06-09 21:12:56 -0300

    Setup for my academic paper

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # graphs_theory
+
+Floyd-Warshall (with path reconstruction) is located between the lines: 1303 and 1381
+
+Dijkstra (with option) is located between the lines: 1125 and 1204
--- main.c
+++ main.c
@@ -3,11 +3,14 @@ int main (int argc, char *argv[])
 {
     _list_header *graph = graph_create_with_time();
     int n = node_counter(graph), w = graph_total_edge_count(graph);
+    int src,dest;
     printf("Number of vertices: %d\nNumber of edges: %d\n\n",n,w);
     puts("Original graph:\n");
     graph_print_with_time(graph);
     puts("\n");
-    _list_header *pathCost = graph_dijkstra_with_option(graph,0,3,COST);
-    _list_header *pathTime = graph_dijkstra_with_option(graph,0,3,TIME);
+    printf("Please enter the source vertex followed by the destination vertex:\n");
+    scanf("%d%d",&src,&dest);
+    _list_header *pathCost = graph_dijkstra_with_option(graph,src,dest,COST);
+    _list_header *pathTime = graph_dijkstra_with_option(graph,src,dest,TIME);
     return 0;
 }
