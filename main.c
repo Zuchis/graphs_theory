6a003dc9d713a1eb478a8eb6aba20f4b01725d48
Udesc <udesc@udesc-HP-Compaq-6005-Pro-MT-PC>
2015-06-17 10:54:44 -0300

    Made an interface for the main

--- main.c
+++ main.c
@@ -12,31 +12,58 @@ int main (int argc, char *argv[])
     graph_print_with_time(graph);
     puts("\n");
     while (1){
-		printf("Please Select an option below:\n 1: Dijkstra\n2:Bellman-Ford\n3:Ford-Fulkerson\n4: Floyd Warshall\n5: Dijsktra for time or cost\n6: Floyd-Warshall for time or cost\n7:Delete Tree");
+		printf("Please Select an option below:\n 1: Dijkstra\n2:Bellman-Ford\n3:Ford-Fulkerson\n4: Floyd Warshall\n5: Dijsktra for time or cost\n6: Floyd-Warshall for time or cost\n7:Delete Tree\n0: Exit\n");
 		scanf("%d",&op);
 		switch (op){
 			case 1:
-				printf("Please Enter the initial vertex:\n"):
+				printf("Please Enter the initial vertex:\n");
 				scanf("%d",&src);
 				tree = graph_dijkstra(graph,src);
 				break;
 			case 2:
-				printf("Please Enter the initial vertex:\n"):
+				printf("Please Enter the initial vertex:\n");
 				scanf("%d",&src);
-			    if(!(ret = graph_bellmanFord(graph,tree,src)
-					printf("The graph has a negative cycle");
+			    if(!(ret = graph_bellmanFord(graph,tree,src)))
+					printf("The graph has a negative cycle\n");
 				break;
 			case 3:
 				printf("Please Enter the source and the sink respectively:\n");
 			    scanf("%d%d",&src,&dest);
 				maxFlow = graph_ford_fulkerson(graph,src,dest);
-				
-					
-		}	
-	}				
-    printf("Please enter the source vertex followed by the destination vertex:\n");
-    scanf("%d%d",&src,&dest);
-    _list_header *pathCost = graph_dijkstra_with_option(graph,src,dest,COST);
-    _list_header *pathTime = graph_dijkstra_with_option(graph,src,dest,TIME);
+                printf("The maximum flow is %d\n",maxFlow);
+                break;
+            case 4:
+                printf("Please enter the initial vertex:\n");
+                scanf("%d",&src);
+                graph_floydWarshall(graph);
+                break;
+            case 5:
+                printf("Please enter the initial and destiny vertex:\n");
+                scanf("%d%d",&src,&dest);
+                printf("Please enter your option (C or T):\n");
+                scanf("%c",&option);
+                if (option == 'C')
+                    tree = graph_floydWarshall_with_path_reconstrution(graph,src,dest,COST);
+                else
+                    tree = graph_floydWarshall_with_path_reconstrution(graph,src,dest,TIME);
+                break;
+            case 6:
+                printf("Please enter the initial and destiny vertex:\n");
+                scanf("%d%d",&src,&dest);
+                printf("Please enter your option (C or T):\n");
+                scanf("%c",&option);
+                if (option == 'C')
+                    tree = graph_dijkstra_with_option(graph,src,dest,COST);
+                else
+                    tree = graph_dijkstra_with_option(graph,src,dest,TIME);
+                break;
+            case 7:
+                list_purge(tree);
+                free(tree);
+                break;
+            default:
+                printf("Not a valid option\n");
+		}
+	}
     return 0;
 }
